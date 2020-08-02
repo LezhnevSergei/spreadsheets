72900bc07827dc95d5ed4d99b25e6c89f32fdfd5
LezhnevSergei <catisfat1337@gmail.com>
2020-08-02 23:40:51 +0500

    Finished base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const path = require('path');
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
 
-const isProd = process.env.NODE_ENV === 'production';
-const isDev = !isProd;
-
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
 const jsLoaders = () => {
   const loaders = [
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties']
       }
     }
-  ];
-  
+  ]
+
   if (isDev) {
-    loaders.push('eslint-loader');
+    loaders.push('eslint-loader')
   }
-  
-  return loaders;
-};
+
+  return loaders
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -39,7 +39,7 @@ module.exports = {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
+      '@core': path.resolve(__dirname, 'src/core')
     }
   },
   devtool: isDev ? 'source-map' : false,
@@ -49,21 +49,19 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
+    new HTMLWebpackPlugin({
       template: 'index.html',
       minify: {
         removeComments: isProd,
         collapseWhitespace: isProd
       }
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
-        }
-      ],
-    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
     new MiniCssExtractPlugin({
       filename: filename('css')
     })
@@ -81,7 +79,7 @@ module.exports = {
             }
           },
           'css-loader',
-          'sass-loader',
+          'sass-loader'
         ],
       },
       {
@@ -89,6 +87,6 @@ module.exports = {
         exclude: /node_modules/,
         use: jsLoaders()
       }
-    ],
+    ]
   }
-};
+}
